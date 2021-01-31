9f9511f73b2776b2209d98c49dbda7f25d268721
SyrusVir <jude.alnas@gmail.com>
2021-01-31 01:44:05 -0600

    Testing complete

--- fifo.h
+++ fifo.h
@@ -7,12 +7,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <errno.h>
 
 typedef struct Node {
-    int priority;
     void* data;
     struct Node *next;
     struct Node *prev;
+    int priority;
 } fifo_node_t;
 
 typedef struct Buffer {
@@ -21,16 +22,13 @@ typedef struct Buffer {
     pthread_cond_t cond_nonempty;
     int max_buffer_size;
     int buffer_occupancy;
-    fifo_node_t *head;
-    fifo_node_t *tail;
+    fifo_node_t *sentinel;
 } fifo_buffer_t;
 
 int fifoPush(fifo_buffer_t* buffer, void* data, int priority, bool blocking);
 void* fifoPull(fifo_buffer_t* buffer, bool blocking);
-void** fifoFlush(fifo_buffer_t* buffer);
-int fifoUpdateOccupancy(fifo_buffer_t* buffer);
-void fifoPrint(fifo_buffer_t* buffer);
+void** fifoFlush(fifo_buffer_t* buffer, bool blocking);
 fifo_buffer_t* fifoBufferInit(int max_buffer_size);
-fifo_node_t* fifoNodeCreate(void* data, int priority);
 void** fifoBufferClose(fifo_buffer_t*);
-void* fifoNodeDestroy(fifo_node_t*);
+void fifoPrint(fifo_buffer_t* buffer);
+int fifoUpdateOccupancy(fifo_buffer_t* buffer);
